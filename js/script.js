da35f7992a93fd2534a8492b8f37bf1095a2bbb3
thomas miño <107081617+miniopa@users.noreply.github.com>
2022-07-05 14:04:55 -0300

    agregando boostrap a header 2.0

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const container_loader = document.querySelector('.container_loader')
+    container_loader.style.opacity = 0
+    container_loader.style.visibility = 'hidden'
+})
